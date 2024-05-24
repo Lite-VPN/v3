48a6d12365de3155e1956df26ff0abbce30aefdc
Lite-VPN <154042720+Lite-VPN@users.noreply.github.com>
2024-05-24 11:47:58 +0800

    Update fixhap.sh

--- files/fixhap.sh
+++ files/fixhap.sh
@@ -58,7 +58,7 @@ clear
 
 
 
-REPO="https://github.com/Lite-VPN/v3/blob/main"
+REPO="https://github.com/Lite-VPN/v3/blob/main/config/haproxy.cfg"
 # fixhap fix Haproxy
 clear
 echo -e "${BlueCyan} ------------------------- ${Xark} "
