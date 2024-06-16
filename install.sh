8047d53160813f813263393185bf99d3e8865964
Lite-VPN <154042720+Lite-VPN@users.noreply.github.com>
2024-06-16 23:16:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,10 @@ apt-get install libjpeg-dev zlib1g-dev -y
 cd /etc/
 git clone https://github.com/Lite-VPN/v3.git
 pip3 install -r /etc/v3/requirements.txt
-### Magelang Phreaker Bash Decryptor 2027
 clear
 read -e -p "[*] Input Nama Luuu   : " nama
 read -e -p "[*] Input Bot Token   : " bottoken
 read -e -p "[*] Input Id Telegram : " admin
-#Decrypted by Yaddy D Phreaker
 sed -i "s/NAMA STORE/$nama/g" /etc/v3/config.json &> /dev/null
 sed -i "s/BOT TOKEN/$bottoken/g" /etc/v3/config.json &> /dev/null
 sed -i "s/ID TELEGRAM/$admin/g" /etc/v3/config.json &> /dev/null
