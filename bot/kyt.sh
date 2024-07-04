9f7293bed17df9e3965c952c8b293dd085b8812c
Lite-VPN <154042720+Lite-VPN@users.noreply.github.com>
2024-07-04 09:33:42 +0800

    Update kyt.sh

--- bot/kyt.sh
+++ bot/kyt.sh
@@ -31,9 +31,9 @@ pip3 install -r kyt/requirements.txt
 
 #isi data
 echo ""
-figlet  LITE VERMILION  | lolcat
+figlet  Telegram  | lolcat
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[1;97;101m          ADD BOT PANEL          \e[0m"
+echo -e " \e[92;1m          Telegram BOT Panel          \e[0m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "${grenbo}Tutorial Creat Bot and ID Telegram${NC}"
 echo -e "${grenbo}[*] Creat Bot and Token Bot : @BotFather${NC}"
